c710a00666ca446a66ecd7f09bba1f16e6dff604
Christopher Grote <chris@thegrotes.net>
2020-09-15 19:32:07 +0100

    Utility scripts for automating CTS testing across versions
    Signed-off-by: Christopher Grote <chris@thegrotes.net>

---
+++ cts/charts/runAll.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+releases=("v11502sp3" "v11502sp5" "v11502sp6" "v11700" "v11701" "v11701sp1" "v11702" "v11710" "v11710sp1" "v11710sp2")
+parallelism=4
+throttle=180
+
+index=0
+running=0
+while [ $index -lt ${#releases[@]} ]; do
+  next_release=${releases[$index]}
+  echo "Starting CTS ($index) for release: $next_release ..."
+  screen -S "cts_$next_release" -d -m ./runAndCollect.sh "$next_release"
+  index=$((index+1))
+  running=$((running+1))
+  echo " ... throttling (delaying) for $throttle seconds"
+  sleep $throttle
+  while [ $running -ge $parallelism ]; do
+    echo " ... maximum parallelism ($parallelism) reached, continuing to throttle"
+    sleep $throttle
+    running=$(screen -ls | grep -c "cts_" || true)
+  done
+done
